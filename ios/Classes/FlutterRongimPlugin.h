b0b7e77b845f0adb5bddf2f9eea1b051bf187ce6
marekchen <c491843272@gmail.com>
2018-10-02 14:43:16 +0800

    first commit

---
+++ ios/Classes/FlutterRongimPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface FlutterRongimPlugin : NSObject<FlutterPlugin>
+@end
---
+++ ios/Classes/FlutterRongimPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterRongimPlugin.h"
+
+@implementation FlutterRongimPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_rongim"
+            binaryMessenger:[registrar messenger]];
+  FlutterRongimPlugin* instance = [[FlutterRongimPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
